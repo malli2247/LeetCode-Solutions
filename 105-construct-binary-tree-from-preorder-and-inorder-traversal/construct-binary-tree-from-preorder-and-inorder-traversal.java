57197459c5c6a207e66d6f343dc5438641d5b0bc
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-07-06 20:32:09 +0530

    Time: 2 ms (64.79%) | Memory: 46.3 MB (63.32%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,32 +1,27 @@
 class Solution {
+    private int preorderIndex;
+    private Map<Integer, Integer> mapping;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Deque<Integer> preorderQueue = new ArrayDeque<>();
-        for (int val : preorder) {
-            preorderQueue.offer(val);
+        mapping = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            mapping.put(inorder[i], i);
         }
 
-        return build(preorderQueue, inorder);        
+        preorderIndex = 0;
+        return build(preorder, 0, inorder.length - 1);        
     }
 
-    private TreeNode build(Deque<Integer> preorder, int[] inorder) {
-        if (inorder.length > 0) {
-            int idx = indexOf(inorder, preorder.poll());
-            TreeNode root = new TreeNode(inorder[idx]);
+    private TreeNode build(int[] preorder, int start, int end) {
+        if (start > end) return null;
 
-            root.left = build(preorder, Arrays.copyOfRange(inorder, 0, idx));
-            root.right = build(preorder, Arrays.copyOfRange(inorder, idx + 1, inorder.length));
+        int rootVal = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = mapping.get(rootVal);
 
-            return root;
-        }
-        return null;
+        root.left = build(preorder, start, mid - 1);
+        root.right = build(preorder, mid + 1, end);
+
+        return root;
     }
-
-    private int indexOf(int[] arr, int value) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == value) {
-                return i;
-            }
-        }
-        return -1; // shouldn't happen with valid input
-    }    
 }
