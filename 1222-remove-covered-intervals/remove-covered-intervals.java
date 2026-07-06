0814813ab6e3e10af9dc71ff99f7b7a4fe52b5c5
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-07-06 20:24:31 +0530

    Time: 6 ms (93.21%) | Memory: 46.7 MB (24.23%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int removeCoveredIntervals(int[][] intervals) {
-
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] != b[0])
-                return a[0] - b[0];
-            return b[1] - a[1];
+    public int removeCoveredIntervals(int[][] in) {
+        Arrays.sort(in, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            return a[1] - b[1];
         });
 
-        int ans = intervals.length;
+        int n = in.length, ans = n;
+        int l = in[0][0], r = in[0][1];
 
-        int low = intervals[0][0];
-        int high = intervals[0][1];
+        if (n == 1) return 1;
 
-        for (int i = 1; i < intervals.length; i++) {
-
-            if (intervals[i][0] == low || intervals[i][1] <= high) {
+        for (int i = 1; i < n; i++) {
+            // Current interval is covered by [l, r]
+            if (r >= in[i][1] && l <= in[i][0]) {
                 ans--;
-            } else {
-                low = intervals[i][0];
-                high = intervals[i][1];
+            }
+            // Same start, larger interval covers previous one
+            else if (l == in[i][0]) {
+                ans--;
+                r = in[i][1];
+            }
+            // Move to the new active interval
+            else {
+                l = in[i][0];
+                r = in[i][1];
             }
         }
 
