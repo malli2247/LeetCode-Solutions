ef7d841a80f19a1482a472e458dc59e0303e8881
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-07-07 12:47:33 +0530

    Time: 70 ms (83.59%) | Memory: 47 MB (90.23%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,37 +1,31 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<>();
-
-        if (num == null || num.length() == 0) {
-            return rst;
-        }
-
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
-    }
-
-    private void helper(List<String> rst, String path, String num,int target, int pos, long eval, long multed) {
-        if (pos == num.length()) {
-            if (target == eval) {
-                rst.add(path);
-            }
-            return;
-        }
-       
-        for (int i = pos; i < num.length(); i++) {
-            if (i != pos && num.charAt(pos) == '0') {
-                break;
-            }
-
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-
-            if (pos == 0) {
-                helper(rst, path + cur, num, target,i + 1, cur, cur);
-            } else {
-                helper(rst, path + "+" + cur, num, target,i + 1, eval + cur, cur);
-                helper(rst, path + "-" + cur, num, target,i + 1, eval - cur, -cur);
-                helper(rst, path + "*" + cur, num, target,i + 1,eval - multed + multed * cur,multed * cur);
-            }
-        }
-    }
+    List<String> res = new ArrayList<>();
+   	StringBuilder sb = new StringBuilder();
+    dfs(res, sb, num, 0, target, 0, 0);
+    return res;
+    
+}
+public void dfs(List<String> res, StringBuilder sb, String num, int pos, int target, long prev, long multi) { 
+	if(pos == num.length()) {
+		if(target == prev) res.add(sb.toString());
+		return;
+	}
+	for(int i = pos; i < num.length(); i++) {
+		if(num.charAt(pos) == '0' && i != pos) break;
+		long curr = Long.parseLong(num.substring(pos, i + 1));
+		int len = sb.length();
+		if(pos == 0) {
+			dfs(res, sb.append(curr), num, i + 1, target, curr, curr); 
+			sb.setLength(len);
+		} else {
+			dfs(res, sb.append("+").append(curr), num, i + 1, target, prev + curr, curr); 
+			sb.setLength(len);
+			dfs(res, sb.append("-").append(curr), num, i + 1, target, prev - curr, -curr); 
+			sb.setLength(len);
+			dfs(res, sb.append("*").append(curr), num, i + 1, target, prev - multi + multi * curr, multi * curr); 
+			sb.setLength(len);
+		}
+	}
+}
 }
