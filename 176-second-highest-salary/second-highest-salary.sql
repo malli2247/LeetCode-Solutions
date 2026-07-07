0982eac06b87456f81b709d8644090bd0af6867a
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-07-07 12:41:23 +0530

    Time: 283 ms (61.92%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,10 @@
-SELECT MAX(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+SELECT (
+    SELECT DISTINCT salary
+    FROM (
+        SELECT 
+            salary,
+            DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+        FROM Employee
+    ) AS RankedSalaries
+    WHERE rnk = 2
+) AS SecondHighestSalary;
