5ba686723d7b91b9f01271aca889473476f5f182
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-08-12 22:42:22 +0530

    Time: 23 ms (28.54%) | Memory: 65 MB (40.43%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -8,6 +8,7 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1]){
                 res.add(nums[i]);
+                i++;
             }
         }
         return res;
