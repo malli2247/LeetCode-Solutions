9d771e33c353aef4ac902f08716017d10258e385
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-08-23 21:32:56 +0530

    Time: 7 ms (79.56%) | Memory: 46.8 MB (91.24%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -7,14 +7,24 @@ class Solution {
         for (int i = 0; i < n; i++) {
             char c = num.charAt(i);
             if (c == '?') {
-                if (i < half) q1++;
-                else q2++;
+                if (i < half){
+                    q1++;
+                }
+                else{
+                    q2++;
+                }
             } else {
-                if (i < half) s1 += (c - '0');
-                else s2 += (c - '0');
+                if (i < half){
+                    s1 += (c - '0');
+                }
+                else{
+                    s2 += (c - '0');
+                }
             }
         }
-        if ((q1 + q2) % 2 != 0) return true;
+        if ((q1 + q2) % 2 != 0){
+            return true;
+            }
         return (2 * s1 + 9 * q1) != (2 * s2 + 9 * q2);
     }
 }
