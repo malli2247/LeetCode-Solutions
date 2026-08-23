521c91d858049ff88a001638b3922710bf9f5541
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-08-23 21:34:48 +0530

    Time: 9 ms (29.93%) | Memory: 47 MB (41.61%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -1,32 +1,22 @@
 class Solution {
     public boolean sumGame(String num) {
-        int n = num.length();
-        int half = n / 2;
-        int s1 = 0, s2 = 0;
-        int q1 = 0, q2 = 0;
-        for (int i = 0; i < n; i++) {
-            char c = num.charAt(i);
-            if (c == '?') {
-                if (i < half){
-                    q1++;
-                }
-                else{
-                    q2++;
-                }
+        int n = num.length() / 2;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < num.length(); i++) {
+            char ch = num.charAt(i);
+            int value;
+            if (ch == '?') {
+                value = 9;
             } else {
-                if (i < half){
-                    s1 += (c - '0');
-                }
-                else{
-                    s2 += (c - '0');
-                }
+                value = 2 * (ch - '0');
+            }
+            if (i < n) {
+                left += value;
+            } else {
+                right += value;
             }
         }
-        if ((q1 + q2) % 2 != 0){
-            return true;
-            }
-        else{
-            return (2 * s1 + 9 * q1) != (2 * s2 + 9 * q2);
-            }
-        }
+        return left != right;
+    }
 }
