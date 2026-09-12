35cc64e6240c5e58de30defffb914eb9ac35d600
Boya Mallikarjuna <123341378+malli2247@users.noreply.github.com>
2026-09-12 23:46:03 +0530

    Time: 174 ms (48.15%) | Memory: 231.7 MB (25.92%) - LeetSync

--- 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.java
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.java
@@ -1,116 +1,81 @@
 class Solution {
-
-    static class Pair {
-        long sum;
-        List<Integer> ids;
-
-        Pair(long sum, List<Integer> ids) {
-            this.sum = sum;
-            this.ids = ids;
-        }
-
-        Pair copy() {
-            return new Pair(sum, new ArrayList<>(ids));
-        }
-    }
-
-    Pair[][] dp;
-    int[] next;
-
-    Pair better(Pair a, Pair b) {
-        if (a.sum != b.sum)
-            return a.sum > b.sum ? a : b;
-
-        Collections.sort(a.ids);
-        Collections.sort(b.ids);
-
-        for (int i = 0; i < Math.min(a.ids.size(), b.ids.size()); i++) {
-            if (!a.ids.get(i).equals(b.ids.get(i)))
-                return a.ids.get(i) < b.ids.get(i) ? a : b;
-        }
-
-        return a.ids.size() <= b.ids.size() ? a : b;
-    }
-
-    int lowerBound(List<List<Integer>> in, List<Integer> order, int target) {
-        int l = 0, r = order.size();
-
-        while (l < r) {
-            int m = l + (r - l) / 2;
-
-            if (in.get(order.get(m)).get(0) >= target)
-                r = m;
-            else
-                l = m + 1;
-        }
-
-        return l;
-    }
-
-    Pair solve(List<List<Integer>> in, List<Integer> order,
-               int pos, int count) {
-
-        if (pos == order.size() || count == 4)
-            return new Pair(0, new ArrayList<>());
-
-        if (dp[pos][count] != null)
-            return dp[pos][count].copy();
-
-        Pair skip = solve(in, order, pos + 1, count);
-
-        int id = order.get(pos);
-
-        Pair take = solve(
-            in,
-            order,
-            next[pos],
-            count + 1
-        );
-
-        take.sum += in.get(id).get(2);
-        take.ids.add(id);
-
-        dp[pos][count] = better(skip, take);
-
-        return dp[pos][count].copy();
-    }
-
     public int[] maximumWeight(List<List<Integer>> intervals) {
         int n = intervals.size();
-
-        List<Integer> order = new ArrayList<>();
-
-        for (int i = 0; i < n; i++)
-            order.add(i);
-
-        order.sort((a, b) ->
-            Integer.compare(
-                intervals.get(a).get(0),
-                intervals.get(b).get(0)
-            )
-        );
-
-        next = new int[n];
-
+        int[][] arr = new int[n][4];
+        
         for (int i = 0; i < n; i++) {
-            int id = order.get(i);
-            next[i] = lowerBound(
-                intervals,
-                order,
-                intervals.get(id).get(1) + 1
-            );
+            arr[i][0] = intervals.get(i).get(0);
+            arr[i][1] = intervals.get(i).get(1);
+            arr[i][2] = intervals.get(i).get(2);
+            arr[i][3] = i;
         }
-
-        dp = new Pair[n][4];
-
-        List<Integer> ans = solve(intervals, order, 0, 0).ids;
-        Collections.sort(ans);
-
-        int[] res = new int[ans.size()];
-
-        for (int i = 0; i < ans.size(); i++)
-            res[i] = ans.get(i);
-
-        return res;
+        
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        long[][] dpWeight = new long[n + 1][5];
+        List<Integer>[][] dpList = new ArrayList[n + 1][5];
+        
+        for (int i = 0; i <= n; i++) 
+            for (int k = 0; k <= 4; k++) 
+                dpList[i][k] = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            int l = arr[i - 1][0];
+            long w = arr[i - 1][2];
+            int id = arr[i - 1][3];
+            
+            int low = 1, high = i - 1, prev = 0;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (arr[mid - 1][1] < l) {
+                    prev = mid;
+                    low = mid + 1;
+                } else 
+                    high = mid - 1;
+            }
+            
+            for (int k = 1; k <= 4; k++) {
+                long skipW = dpWeight[i - 1][k];
+                List<Integer> skipList = dpList[i - 1][k];
+                
+                long takeW = dpWeight[prev][k - 1] + w;
+                List<Integer> takeList = new ArrayList<>(dpList[prev][k - 1]);
+                takeList.add(id);
+                Collections.sort(takeList);
+                
+                if (takeW > skipW) {
+                    dpWeight[i][k] = takeW;
+                    dpList[i][k] = takeList;
+                } else if (takeW == skipW) {
+                    if (isLexicographicallySmaller(takeList, skipList)) {
+                        dpWeight[i][k] = takeW;
+                        dpList[i][k] = takeList;
+                    } else {
+                        dpWeight[i][k] = skipW;
+                        dpList[i][k] = skipList;
+                    }
+                } else {
+                    dpWeight[i][k] = skipW;
+                    dpList[i][k] = skipList;
+                }
+            }
+        }
+        
+        List<Integer> resultList = dpList[n][4];
+        int[] result = new int[resultList.size()];
+        
+        for (int i = 0; i < resultList.size(); i++) 
+            result[i] = resultList.get(i);
+        
+        return result;
+    }
+    
+    private boolean isLexicographicallySmaller(List<Integer> a, List<Integer> b) {
+        int size = Math.min(a.size(), b.size());
+        for (int i = 0; i < size; i++) 
+            if (!a.get(i).equals(b.get(i))) 
+                return a.get(i) < b.get(i);
+                
+        return a.size() < b.size();
     }
 }
